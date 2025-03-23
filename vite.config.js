e787717f266a00a5df922c2000daf65ace4521d3
Firoz Akhter <firozakhter789@gmail.comgit config --list>
2025-03-23 22:20:09 +0530

    changes to fix npm run build error

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 });
-
-module.exports = {
-  extend: {
-    animation: {
-      marquee: "marquee 25s linear infinite",
-      marquee2: "marquee2 25s linear infinite",
-    },
-    keyframes: {
-      marquee: {
-        "0%": { transform: "translateX(0%)" },
-        "100%": { transform: "translateX(-100%)" },
-      },
-      marquee2: {
-        "0%": { transform: "translateX(100%)" },
-        "100%": { transform: "translateX(0%)" },
-      },
-    },
-  },
-};
