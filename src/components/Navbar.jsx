e0c22faccb2831a3a2b8e1aa6ade6a0233f0c67f
firoz akhter <firozakhter789@gmail.com>
2025-03-30 11:53:16 +0530

    navigate to login now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -213,48 +213,52 @@ const Navbar = () => {
           <span className="px-3">About Us</span>
         </Link>
 
-        <button className="bg-white cursor-pointer text-blue-500 rounded-md px-4 py-3 flex items-center gap-2 hover:bg-blue-50 transition-colors">
-          <svg
-            viewBox="0 0 24 24"
-            height="20px"
-            width="20px"
-            aria-hidden="true"
-            focusable="false"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            className="text-blue-500"
-          >
-            <g fill="none">
-              <path d="M0 0h24v24H0V0z"></path>
-              <path d="M0 0h24v24H0V0z" opacity="0.87"></path>
-            </g>
-            <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
-          </svg>
-          Login Now
-        </button>
+        <Link to="/login">
+          <button className="bg-white cursor-pointer text-blue-500 rounded-md px-4 py-3 flex items-center gap-2 hover:bg-blue-50 transition-colors">
+            <svg
+              viewBox="0 0 24 24"
+              height="20px"
+              width="20px"
+              aria-hidden="true"
+              focusable="false"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              className="text-blue-500"
+            >
+              <g fill="none">
+                <path d="M0 0h24v24H0V0z"></path>
+                <path d="M0 0h24v24H0V0z" opacity="0.87"></path>
+              </g>
+              <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
+            </svg>
+            Login Now
+          </button>
+        </Link>
       </div>
 
       {/* Only show login button on mobile */}
       <div className="flex md:hidden">
-        <button className="bg-white cursor-pointer text-blue-500 rounded-md px-4 py-3 flex items-center gap-2 hover:bg-blue-50 transition-colors">
-          <svg
-            viewBox="0 0 24 24"
-            height="20px"
-            width="20px"
-            aria-hidden="true"
-            focusable="false"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            className="text-blue-500"
-          >
-            <g fill="none">
-              <path d="M0 0h24v24H0V0z"></path>
-              <path d="M0 0h24v24H0V0z" opacity="0.87"></path>
-            </g>
-            <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
-          </svg>
-          Login Now
-        </button>
+        <Link to="/login">
+          <button className="bg-white cursor-pointer text-blue-500 rounded-md px-4 py-3 flex items-center gap-2 hover:bg-blue-50 transition-colors">
+            <svg
+              viewBox="0 0 24 24"
+              height="20px"
+              width="20px"
+              aria-hidden="true"
+              focusable="false"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              className="text-blue-500"
+            >
+              <g fill="none">
+                <path d="M0 0h24v24H0V0z"></path>
+                <path d="M0 0h24v24H0V0z" opacity="0.87"></path>
+              </g>
+              <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
+            </svg>
+            Login Now
+          </button>
+        </Link>
       </div>
     </nav>
   );
